abe73f9539099c8f5352548885ac218acefa5018
antbessonov <anton@bessonov.org>
2022-09-06 20:52:52 +0700

    CoinInfoDbModel

---
+++ app/src/main/java/com/example/roomdbtest/database/CoinInfoDbModel.kt
@@ -0,0 +1,16 @@
+package com.example.roomdbtest.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "crypto_coins")
+data class CoinInfoDbModel(
+    @PrimaryKey
+    val id: Int?,
+    val name: String?,
+    val symbol: String?,
+    val cmcRank: Int?,
+    val tags: String?,
+    val lastUpdated: String?,
+    val price: Double?,
+)
