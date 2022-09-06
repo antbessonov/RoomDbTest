d968bb24c8032b9d0d2d563c2f7111ae2894830a
antbessonov <anton@bessonov.org>
2022-09-06 21:02:36 +0700

    AppDatabaseTest

---
+++ app/src/androidTest/java/com/example/roomdbtest/database/AppDatabaseTest.kt
@@ -0,0 +1,55 @@
+package com.example.roomdbtest.database
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.roomdbtest.tools.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppDatabaseTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var db: AppDatabase
+    private lateinit var dao: CoinInfoDao
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        dao = db.coinsInfoDao()
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun insertAndReadCoinInfoListDbModel() = runBlocking {
+        val coinInfoListDbModel = listOf(CoinInfoDbModel(
+            id = 1,
+            name = "Bitcoin",
+            symbol = "BTC",
+            cmcRank = 1,
+            tags = "coin",
+            lastUpdated = "2022-09-02T10:10:00.000Z",
+            price = 19523.1234
+        ))
+        dao.insertCoinInfoList(coinInfoListDbModel)
+        val coinInfoListDbModelInDb = dao.getCoinInfoList().getOrAwaitValue()
+        assertThat(coinInfoListDbModelInDb.containsAll(coinInfoListDbModel)).isTrue()
+    }
+
+}
